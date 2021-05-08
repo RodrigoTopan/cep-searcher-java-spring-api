b2cbb4c142a821c6077c61008e44dd407f3103fc
RodrigoTopan <rodrigo.topan.ti@gmail.com>
2021-05-08 14:12:35 -0300

    core: initial commit with project structure

---
+++ src/main/java/cepsearcher/CepsearcherApplication.java
@@ -0,0 +1,13 @@
+package cepsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepsearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepsearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cepsearcher/CepsearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package cepsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepsearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
